c12c41f34e2eda43740cb966f9fa0d2de684fc5f
Ro <u202213765@upc.edu.pe>
2025-09-02 16:27:52 -0500

    chore: initial commit

---
+++ src/main/java/com/entrena/entrenaplatform/EntrenaPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.entrena.entrenaplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrenaPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntrenaPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/entrena/entrenaplatform/EntrenaPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entrena.entrenaplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrenaPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
